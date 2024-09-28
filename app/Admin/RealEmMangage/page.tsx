1c106cf143f9724bf2b3795710ae1452d7583411
Tedokoji <tedokojikagami@gmail.com>
2024-09-28 13:50:47 +0700

    table for employee test, damn i got no time to polish anything

--- app/Admin/RealEmMangage/page.tsx
+++ app/Admin/RealEmMangage/page.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import Grade from "../Findidk/Grade";
 import { Person } from "../Findidk/page";
 import BackButton from "@/public/Buttons/BackButton";
-
+import './Realem.scss'
 const RealEM = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: Person[] = await res.json();
   return (
     <>
@@ -19,31 +19,29 @@ const RealEM = async () => {
               <table className="bordered">
                 <thead>
                   <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Days Worked</th>
-                    <th>Status</th>
-                    <th>Actions</th>
+                    <th className="titletable">ID</th>
+                    <th className="titletable">Name</th>
+                    <th className="titletable">Email</th>
+                    <th className="titletable">Days Worked</th>
+                    <th className="titletable">Status</th>
+                    <th className="titletable">Actions</th>
                   </tr>
                 </thead>
                 <tbody>
-                  
                   {users.map((user) => (
-          <tr>
-          <td>{user.id}</td>
-          <td>{user.name}</td>
-          <td>{user.email}</td>
-          <td>50</td>
-          <td>Active</td>
-          <td>
-            <button>Edit</button> <button>Delete</button>
-          </td>
-        </tr>
-        ))}
+                    <tr className="table-row">
+                      <td>{user.id}</td>
+                      <td>{user.name}</td>
+                      <td>{user.email}</td>
+                      <td>50</td>
+                      <td>Active</td>
+                      <td>
+                        <button>Edit</button> <button>Delete</button>
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
-              
             </div>
             <br></br>
           </div>
